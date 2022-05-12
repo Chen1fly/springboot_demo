6572fc78307ca67e039afc20bac5faac21bbe204
Chen1fly <77045860+Chen1fly@users.noreply.github.com>
2022-05-12 12:56:19 +0800

    Add files via upload

---
+++ src/main/resources/com/cyf/sql/ems_thyemleaf.sql
@@ -0,0 +1,103 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 阿里云毕设数据库
+ Source Server Type    : MySQL
+ Source Server Version : 50558
+ Source Host           : 121.199.62.19:3306
+ Source Schema         : ems_thyemleaf
+
+ Target Server Type    : MySQL
+ Target Server Version : 50558
+ File Encoding         : 65001
+
+ Date: 12/05/2022 12:54:42
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for Sys_mumber
+-- ----------------------------
+DROP TABLE IF EXISTS `Sys_mumber`;
+CREATE TABLE `Sys_mumber`  (
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `job` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for Sys_super
+-- ----------------------------
+DROP TABLE IF EXISTS `Sys_super`;
+CREATE TABLE `Sys_super`  (
+  `statu` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`statu`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for article_a
+-- ----------------------------
+DROP TABLE IF EXISTS `article_a`;
+CREATE TABLE `article_a`  (
+  `student_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `article_a` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `article_a_statu` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `statement` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`student_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for article_b
+-- ----------------------------
+DROP TABLE IF EXISTS `article_b`;
+CREATE TABLE `article_b`  (
+  `student_id` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `statu` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`student_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for article_c
+-- ----------------------------
+DROP TABLE IF EXISTS `article_c`;
+CREATE TABLE `article_c`  (
+  `teacher_id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `student_id` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `statu` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`student_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for teacher_subject
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_subject`;
+CREATE TABLE `teacher_subject`  (
+  `teacher_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher_subject` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `student_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `sub_status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `statu` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Triggers structure for table Sys_mumber
+-- ----------------------------
+DROP TRIGGER IF EXISTS `teacher_into_subject`;
+delimiter ;;
+CREATE TRIGGER `teacher_into_subject` AFTER INSERT ON `Sys_mumber` FOR EACH ROW BEGIN
+	INSERT INTO teacher_subject(teacher_subject.teacher_id)
+		SELECT Sys_mumber.username FROM Sys_mumber WHERE Sys_mumber.job='教师';
+		END
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
