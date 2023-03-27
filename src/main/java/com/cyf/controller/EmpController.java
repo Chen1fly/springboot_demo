a28cd5228ed98c49aed15feb3752878b915acf29
Chen <chenyi.jug@gmail.com>
2023-03-28 00:09:30 +0800

    复习

--- src/main/java/com/cyf/controller/EmpController.java
+++ src/main/java/com/cyf/controller/EmpController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+//希望我复试顺利！！
 @Controller
 @RequestMapping(value = "/emp")
 public class EmpController {
